a022a6b9eb7252973a648b350eed4fd89809c342
jupiter002 <jik7194@naver.com>
2023-06-26 14:40:43 +0900

    logging 사용하기

--- src/main/java/jupiter/hello/spring4/controller/IndexController.java
+++ src/main/java/jupiter/hello/spring4/controller/IndexController.java
@@ -1,16 +1,24 @@
 package jupiter.hello.spring4.controller;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+
 @Controller
 public class IndexController {
+
+    // 로거 설정: getLogger(로깅할클래스명)
+    private Logger logger = LogManager.getLogger(IndexController.class);
     // http://localhost:8080/ 호출시 실행
     @RequestMapping("/")
     public String index(Model m){
         // addAttrubute(변수명, 대상객체)
         m.addAttribute("sayHello","Hello World! from Controller");
-
+        //로거 출력
+        logger.info("Hello,world !! from logger");
         return "index";
     }
 }
