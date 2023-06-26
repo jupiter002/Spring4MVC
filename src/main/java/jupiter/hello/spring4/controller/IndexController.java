a99df16a019ac7e4be78019abcfadc052e8b79c7
jupiter002 <jik7194@naver.com>
2023-06-26 13:53:31 +0900

    IndexController에 모델 객체생성 sayHello 변수 추가

--- src/main/java/jupiter/hello/spring4/controller/IndexController.java
+++ src/main/java/jupiter/hello/spring4/controller/IndexController.java
@@ -1,12 +1,15 @@
 package jupiter.hello.spring4.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController {
     // http://localhost:8080/ 호출시 실행
     @RequestMapping("/")
-    public String index(){
+    public String index(Model m){
+        // addAttrubute(변수명, 대상객체)
+        m.addAttribute("sayHello","Hello World! from Controller");
 
         return "index";
     }
