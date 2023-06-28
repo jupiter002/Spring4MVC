53c39d5848586a528909bc8e4c06789607a28c92
jupiter002 <jik7194@naver.com>
2023-06-28 18:04:28 +0900

    회원가입 처리 - service

---
+++ src/main/java/jupiter/hello/spring4/service/MemberService.java
@@ -0,0 +1,9 @@
+package jupiter.hello.spring4.service;
+
+import jupiter.hello.spring4.model.Member;
+
+public interface MemberService {
+
+    boolean saveMemeber(Member m);
+
+}
---
+++ src/main/java/jupiter/hello/spring4/service/MemberServiceImpl.java
@@ -0,0 +1,19 @@
+package jupiter.hello.spring4.service;
+
+import jupiter.hello.spring4.dao.MemberDao;
+import jupiter.hello.spring4.model.Member;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service("msrv")
+public class MemberServiceImpl implements MemberService{
+
+    @Autowired
+    private MemberDao mdao;
+    @Override
+    public boolean saveMemeber(Member m) {
+        boolean isSaved = false;
+        if (mdao.insertMember(m) > 0) isSaved = true;
+        return isSaved;
+    }
+}
