1fd5d773a2bd2f9abbc0bd3d87e12607981f2c53
jupiter002 <jik7194@naver.com>
2023-06-28 18:04:02 +0900

    회원가입 처리 - controller

--- src/main/java/jupiter/hello/spring4/controller/MemberController.java
+++ src/main/java/jupiter/hello/spring4/controller/MemberController.java
@@ -1,7 +1,10 @@
 package jupiter.hello.spring4.controller;
 
+import jupiter.hello.spring4.model.Member;
+import jupiter.hello.spring4.service.MemberService;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,6 +16,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class MemberController {
 
     private Logger logger = LogManager.getLogger(MemberController.class);
+    @Autowired
+    MemberService msrv;
 
     @RequestMapping(value = "/member/join", method = RequestMethod.GET)
     public String join(Model m) {
@@ -21,10 +26,13 @@ public class MemberController {
         return "member/join.tiles";
     }
     @RequestMapping(value = "/member/join", method = RequestMethod.POST)
-    public String joinok(Model m) {
+    public String joinok(Member m) {
         logger.info("member/joinok 호출");
+        String viewName = "/member/fail";
 
-        return "redirect:/member/login";
+        if(msrv.saveMemeber(m))
+            viewName = "redirect:/member/login";
+        return viewName;
     }
 
     @RequestMapping(value = "/member/login", method = RequestMethod.GET)
