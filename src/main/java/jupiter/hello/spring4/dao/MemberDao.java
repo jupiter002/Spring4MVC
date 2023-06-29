fedc1d56c55ec712d1d3fd45086062c40e2df475
jupiter002 <jik7194@naver.com>
2023-06-29 15:16:11 +0900

    MemberDao loginMember 구현

--- src/main/java/jupiter/hello/spring4/dao/MemberDao.java
+++ src/main/java/jupiter/hello/spring4/dao/MemberDao.java
@@ -4,4 +4,5 @@ import jupiter.hello.spring4.model.Member;
 
 public interface MemberDao {
     int insertMember(Member m);
+    Member loginMember(Member m);
 }
--- src/main/java/jupiter/hello/spring4/dao/MemberDaoImpl.java
+++ src/main/java/jupiter/hello/spring4/dao/MemberDaoImpl.java
@@ -4,12 +4,17 @@ import jupiter.hello.spring4.model.Member;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 @Repository("mdao")
 public class MemberDaoImpl implements MemberDao{
     //sql.properties에 작성한sql불러오기
     @Value("#{sql['insertMember']}") private String insertSQL;     //@Value
+    @Value("#{sql['loginMember']}") private String loginSQL;     //@Value
 
     @Autowired
     JdbcTemplate jdbcTemplate;
@@ -19,10 +24,34 @@ public class MemberDaoImpl implements MemberDao{
         //매개변수 정의
         Object[] params = new Object[]{
                 m.getUserid(), m.getPasswd(),
-                m.getUsername(), m.getUseremail()
+                m.getName(), m.getEmail()
 
         };
         //쿼리 실행 : update(sql문, 매개변수)
         return jdbcTemplate.update(insertSQL,params);
+    }
+
+    public Member loginMember(Member m) {
+        //매개변수 정의
+        Object[] params = new Object[]{
+                m.getUserid(), m.getPasswd()
+
+        };
+        // 로그인 매퍼 선언 = 콜백함수
+        RowMapper<Member> mapper = new LoginMapper();       // 무슨일이 일어나면 mapper가 처리할것
+
+        //쿼리 실행 : queryForObject(sql문, 매개변수,매퍼) - 단일값 반환
+        return jdbcTemplate.queryForObject(loginSQL,params,mapper);
+    }
+
+    private class LoginMapper implements RowMapper<Member> {
+        @Override
+        public Member mapRow(ResultSet rs, int num) throws SQLException {
+            Member m = new Member();
+            m.setUserid(rs.getString(1));
+            m.setName(rs.getString(2));
+
+            return m;
+        }
     }
 }
