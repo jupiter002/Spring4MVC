24d5d2dbf31d03461c7329050921d962d6c839d9
jupiter002 <jik7194@naver.com>
2023-06-29 16:49:30 +0900

    MemberDao selectOneMember 구현

--- src/main/java/jupiter/hello/spring4/dao/MemberDao.java
+++ src/main/java/jupiter/hello/spring4/dao/MemberDao.java
@@ -5,4 +5,5 @@ import jupiter.hello.spring4.model.Member;
 public interface MemberDao {
     int insertMember(Member m);
     Member loginMember(Member m);
+    Member selectOneMember(String userid);
 }
--- src/main/java/jupiter/hello/spring4/dao/MemberDaoImpl.java
+++ src/main/java/jupiter/hello/spring4/dao/MemberDaoImpl.java
@@ -11,10 +11,14 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 @Repository("mdao")
-public class MemberDaoImpl implements MemberDao{
+public class MemberDaoImpl implements MemberDao {
     //sql.properties에 작성한sql불러오기
-    @Value("#{sql['insertMember']}") private String insertSQL;     //@Value
-    @Value("#{sql['loginMember']}") private String loginSQL;     //@Value
+    @Value("#{sql['insertMember']}")
+    private String insertSQL;     //@Value
+    @Value("#{sql['loginMember']}")
+    private String loginSQL;     //@Value
+    @Value("#{sql['selectOneMember']}")
+    private String selectOneSQL;
 
     @Autowired
     JdbcTemplate jdbcTemplate;
@@ -28,12 +32,12 @@ public class MemberDaoImpl implements MemberDao{
 
         };
         //쿼리 실행 : update(sql문, 매개변수)
-        return jdbcTemplate.update(insertSQL,params);
+        return jdbcTemplate.update(insertSQL, params);
     }
 
     public Member loginMember(Member m) {
         //매개변수 정의
-        Object[] params = new Object[]{
+        Object[] params = new Object[]{         // 배열로 넘어오는 값을 Object[] 변수로 받음
                 m.getUserid(), m.getPasswd()
 
         };
@@ -41,7 +45,8 @@ public class MemberDaoImpl implements MemberDao{
         RowMapper<Member> mapper = new LoginMapper();       // 무슨일이 일어나면 mapper가 처리할것
 
         //쿼리 실행 : queryForObject(sql문, 매개변수,매퍼) - 단일값 반환
-        return jdbcTemplate.queryForObject(loginSQL,params,mapper);
+        m = jdbcTemplate.queryForObject(loginSQL, params, mapper);
+        return m;
     }
 
     private class LoginMapper implements RowMapper<Member> {
@@ -51,6 +56,31 @@ public class MemberDaoImpl implements MemberDao{
             m.setUserid(rs.getString(1));
             m.setName(rs.getString(2));
 
+            return m;
+        }
+    }
+
+    public Member selectOneMember(String userid){
+
+        Object[] params = new Object[]{
+                userid
+        };
+        RowMapper<Member> mapper = new MemberMapper();
+
+        return jdbcTemplate.queryForObject(
+                selectOneSQL, params, mapper);
+
+    }
+
+
+    private class MemberMapper implements RowMapper<Member> {
+        @Override
+        public Member mapRow(ResultSet rs, int num) throws SQLException {
+            Member m = new Member(
+            rs.getString(1), rs.getString(3),
+                    null,rs.getString(4),
+                    rs.getString(5),rs.getString(6)
+            );
             return m;
         }
     }
