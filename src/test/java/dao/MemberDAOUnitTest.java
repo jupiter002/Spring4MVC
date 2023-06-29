461f2e32bc722ea178b7f9b9e5a8c4a600942e0f
jupiter002 <jik7194@naver.com>
2023-06-29 15:16:47 +0900

    MemberDao loginMember 단위테스트

--- src/test/java/dao/MemberDAOUnitTest.java
+++ src/test/java/dao/MemberDAOUnitTest.java
@@ -11,6 +11,7 @@ import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 
 @RunWith(SpringJUnit4ClassRunner.class)     // junit spring 연동
@@ -24,9 +25,18 @@ public class MemberDAOUnitTest {
     @Test           // insertMember 메서드 모의 실행
     @Transactional          // 데이터 입력 후 자동 삭제
     public void insertMember() throws Exception{
-        // assertEquals(테스트메서드, 검증값)
         Member m = new Member(null, "abc123a","987xyz","abc123","abc123@987xyz.co.kr",null);
+        // assertEquals(테스트메서드, 검증값)
         assertEquals(mdao.insertMember(m),1);
+    }
+    @Test           // loginMember 메서드 모의 실행
+    public void loginMember() throws Exception{
+        Member m = new Member();
+        m.setUserid("abc123");
+        m.setPasswd("987xyz");
+        // assertEquals(테스트메서드, 검증값)
+        //System.out.println(mdao.loginMember(m));
+        assertNotNull(mdao.loginMember(m));     // 넘어오는 값이 null인지 확인하는 메서드
     }
 
 
