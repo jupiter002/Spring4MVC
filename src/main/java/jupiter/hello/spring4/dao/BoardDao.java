d1c5194889483059bdb9c0a8caf05fd780ff5332
jupiter002 <jik7194@naver.com>
2023-06-30 16:52:27 +0900

    BoardDao : selectBoard 구현

---
+++ src/main/java/jupiter/hello/spring4/dao/BoardDao.java
@@ -0,0 +1,12 @@
+package jupiter.hello.spring4.dao;
+
+import jupiter.hello.spring4.model.Board;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository("bdao")
+public interface BoardDao {
+
+    List<Board> selectBoard(int snum);
+}
---
+++ src/main/java/jupiter/hello/spring4/dao/BoardDaoImpl.java
@@ -0,0 +1,37 @@
+package jupiter.hello.spring4.dao;
+
+import jupiter.hello.spring4.model.Board;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+@Repository("bdao")
+public class BoardDaoImpl implements BoardDao{
+    @Value("#{sql['selectBoard']}") private String selectSQL;
+
+    @Autowired JdbcTemplate jdbcTemplate;
+    @Override
+    public List<Board> selectBoard(int snum) {
+        Object[] params = new Object[]{snum};
+
+        RowMapper<Board> mapper = new SelectMapper();
+
+        return jdbcTemplate.query(selectSQL,params,mapper);
+    }
+
+    private class SelectMapper implements RowMapper<Board> {
+        @Override
+        public Board mapRow(ResultSet rs, int num) throws SQLException {
+            Board bd = new Board(rs.getString(1),rs.getString(2),
+                    rs.getString(3),rs.getString(4),
+                    rs.getString(5),"0");
+            return bd;
+        }
+    }
+}
