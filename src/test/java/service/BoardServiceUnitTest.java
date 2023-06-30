2ee5aeda7ae230c6660afa15fad6e8f19d3185e7
jupiter002 <jik7194@naver.com>
2023-06-30 16:54:06 +0900

    BoardService : readBoard 단위테스트

---
+++ src/test/java/service/BoardServiceUnitTest.java
@@ -0,0 +1,36 @@
+package service;
+
+import jupiter.hello.spring4.model.Board;
+import jupiter.hello.spring4.service.BoardService;
+import jupiter.hello.spring4.service.MemberService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({"classpath:spring/servlet-context.xml",
+        "classpath:spring/root-context.xml"})
+@WebAppConfiguration()
+public class BoardServiceUnitTest {
+    @Autowired
+    BoardService bsrv;
+
+
+    @Test
+    public void readBoard() throws Exception{
+        int cpage = 1;
+        List<Board> results = bsrv.readBoard(cpage);
+        assertEquals(results.size(), 15);
+        System.out.println(results);
+
+    }
+
+
+}
