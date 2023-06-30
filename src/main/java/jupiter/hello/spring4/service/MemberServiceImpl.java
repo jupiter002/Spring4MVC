e9eedbb096748dd7dfc590096e24878531e1c643
jupiter002 <jik7194@naver.com>
2023-06-30 10:02:45 +0900

    MemberController : loginfail 매핑 추가

--- src/main/java/jupiter/hello/spring4/service/MemberServiceImpl.java
+++ src/main/java/jupiter/hello/spring4/service/MemberServiceImpl.java
@@ -13,18 +13,18 @@ public class MemberServiceImpl implements MemberService{
     @Override
     public boolean saveMemeber(Member m) {
         boolean isSaved = false;
-        if (mdao.insertMember(m) > 0) isSaved = true;
+        if (mdao.insertMember(m) > 0) isSaved = true;           //쿼리문으로 물어온 값에 들어있나 확인
         return isSaved;
     }
     @Override
     public boolean loginMember(Member m) {
         boolean isLogin = false;
-        if(mdao.loginMember(m) !=null )isLogin = true;
+        if(mdao.loginMember(m) !=null )isLogin = true;          //쿼리문으로 물어온 값에 들어있나 확인
         return isLogin;
     }
 
     @Override
-    public Member readOneMember(String userid) {
+    public Member readOneMember(String userid) {                    //로그인이 성공했을때는 쿼리문으로 물어온 값의 여부를 확인할 필요가 없기때문에 확인x
         return mdao.selectOneMember(userid);
     }
 }
