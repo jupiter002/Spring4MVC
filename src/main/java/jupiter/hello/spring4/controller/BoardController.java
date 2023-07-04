804173841d46a7e30bda0a801eb81d3d97b3e541
jupiter002 <jik7194@naver.com>
2023-07-04 09:56:03 +0900

    pjp에 전달할 세션객체 선언 누락된 logger 추가

--- src/main/java/jupiter/hello/spring4/controller/BoardController.java
+++ src/main/java/jupiter/hello/spring4/controller/BoardController.java
@@ -10,7 +10,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.servlet.http.HttpSession;
 
@@ -33,7 +32,7 @@ public class BoardController {
         return "board/list.tiles";
     }
     @GetMapping("/write")
-    public String write(Model m){
+    public String write(Model m, HttpSession sess){
 
         logger.info("board/write호출");
 
